90856bb7c7fd7970da96f1c8a36a088710e00d0b
Eduardo Siqueira <edugauw@gmail.com>
2023-02-22 22:30:31 -0300

    Primeiro Commit

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+const path = required('path');
+const { VueLoaderPlugin } = required('vue-loader');
+const MiniCssExtractPlugin = required('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/main.js'
+}
